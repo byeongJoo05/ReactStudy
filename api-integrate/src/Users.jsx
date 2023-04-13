6e75f5d3e4cc57431595edfc3c1c5c0c52975137
SonByeongJoo <thsqudwn05@gmail.com>
2023-04-13 18:40:49 +0900

    study: 리액트 공부 react 라우터 공부

--- api-integrate/src/Users.jsx
+++ api-integrate/src/Users.jsx
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import {useAsync} from 'react-async';
 import User from './User';
+
+// 놀랍게 async-react는 현재 사용하지 않는다! 머선일이고!
+// 라우터나 공부하자......
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
@@ -14,13 +17,13 @@ async function getUsers() {
 
 function Users() {
     const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(getUsers, [], true);
+  const {data: users, error, isLoading, run} = useAsync({
+    deferFn: getUsers
+  })
 
-  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
-
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
   return (
     <>
       <ul>
@@ -34,7 +37,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={run}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
